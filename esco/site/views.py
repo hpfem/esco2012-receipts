11dd9617f56cd7968a3ed1ed000c858133f24a63
Mateusz Paprocki <mattpap@gmail.com>
2009-08-27 09:55:34 +0200

    Implemented preliminary set of views for ESCO site

--- esco/site/views.py
+++ esco/site/views.py
@@ -4,32 +4,59 @@ from django.http import HttpResponse, Http404
 from django.template import Context, loader
 
 from django.shortcuts import (
-    get_object_or_404, get_list_or_404,
+    get_object_or_404, get_list_or_404, render_to_response,
 )
 
 urlpatterns = patterns('esco.site.views',
-    (r'^$', 'index'),
+    (r'^$', 'index_view'),
 
-    #(r'^login/$', ),
-    #(r'^logout/$', ),
-    #(r'^register/$', ),
+    (r'^login/$', 'login_view'),
+    (r'^logout/$', 'logout_view'),
+    (r'^register/$', 'register_view'),
 
-    #(r'^home/$', ),
-    #(r'^committees/$', ),
-    #(r'^invited_speakers/$', ),
-    #(r'^call_for_papers/$', ),
-    #(r'^programme/$', ),
-    #(r'^payment/$', ),
-    #(r'^accomodation/$', ),
-    #(r'^travel_information/$', ),
+    (r'^topics/$', 'topics_view'),
+    (r'^keynote/$', 'keynote_view'),
+    (r'^committees/$', 'committees_view'),
+    (r'^dates/$', 'dates_view'),
+    (r'^venue/$', 'venue_view'),
+    (r'^payment/$', 'payment_view'),
+    (r'^accommodation/$', 'accommodation_view'),
+    (r'^travel/$', 'travel_view'),
 )
 
-def index(request):
-    template = loader.get_template('index.html')
+def index_view(request):
+    return render_to_response('base.html', {}, mimetype="application/xhtml+xml")
 
-    c = Context({
+def login_view(request):
+    pass
 
-    })
+def logout_view(request):
+    pass
 
-    return HttpResponse(template.render(c))
+def register_view(request):
+    pass
+
+def topics_view(request):
+    return render_to_response('topics.html', {}, mimetype="application/xhtml+xml")
+
+def keynote_view(request):
+    return render_to_response('keynote.html', {}, mimetype="application/xhtml+xml")
+
+def committees_view(request):
+    return render_to_response('committees.html', {}, mimetype="application/xhtml+xml")
+
+def dates_view(request):
+    return render_to_response('dates.html', {}, mimetype="application/xhtml+xml")
+
+def venue_view(request):
+    return render_to_response('venue.html', {}, mimetype="application/xhtml+xml")
+
+def payment_view(request):
+    return render_to_response('payment.html', {}, mimetype="application/xhtml+xml")
+
+def accommodation_view(request):
+    return render_to_response('accommodation.html', {}, mimetype="application/xhtml+xml")
+
+def travel_view(request):
+    return render_to_response('travel.html', {}, mimetype="application/xhtml+xml")
 
