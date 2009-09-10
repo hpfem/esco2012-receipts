f425de62372fc3dced3b2afff0f80644cda971a0
Mateusz Paprocki <mattpap@gmail.com>
2009-09-10 20:30:12 +0200

    Simplified forms logic (templates and views)

--- esco/site/views.py
+++ esco/site/views.py
@@ -22,45 +22,54 @@ import hashlib
 import datetime
 
 urlpatterns = patterns('esco.site.views',
-    (r'^$', 'index_view'),
+    (r'^$',      'index_view'),
     (r'^home/$', 'index_view'),
 
-    (r'^topics/$', 'topics_view'),
-    (r'^keynote/$', 'keynote_view'),
-    (r'^committees/$', 'committees_view'),
-    (r'^submission/$', 'submission_view'),
-    (r'^venue/$', 'venue_view'),
-    (r'^payment/$', 'payment_view'),
-    (r'^accommodation/$', 'accommodation_view'),
-    (r'^travel/$', 'travel_view'),
+    (r'^topics/$',        '_render_template', {'template': 'content/topics.html'}),
+    (r'^keynote/$',       '_render_template', {'template': 'content/keynote.html'}),
+    (r'^committees/$',    '_render_template', {'template': 'content/committees.html'}),
+    (r'^submission/$',    '_render_template', {'template': 'content/submission.html'}),
+    (r'^payment/$',       '_render_template', {'template': 'content/payment.html'}),
+    (r'^venue/$',         '_render_template', {'template': 'content/venue.html'}),
+    (r'^accommodation/$', '_render_template', {'template': 'content/accommodation.html'}),
+    (r'^travel/$',        '_render_template', {'template': 'content/travel.html'}),
 
     (r'^account/login/$', 'account_login_view'),
     (r'^account/logout/$', 'account_logout_view'),
 
-    (r'^account/delete/$', 'account_delete_view'),
-    (r'^account/delete/success/$', 'account_delete_success_view'),
-
     (r'^account/create/$', 'account_create_view'),
-    (r'^account/create/success/$', 'account_create_success_view'),
+    (r'^account/create/success/$', 'account_login_view',
+        {'message': 'New account was created. You can login now.'}),
+
+    (r'^account/delete/$', 'account_delete_view'),
+    (r'^account/delete/success/$', 'index_view',
+        {'message': 'Your account was successfully removed.'}),
 
     (r'^account/password/change/$', 'account_password_change_view'),
-    (r'^account/password/change/success/$', 'account_password_change_success_view'),
+    (r'^account/password/change/success/$', 'index_view',
+        {'message': 'Your password was successfully changed.'}),
 
     (r'^account/password/remind/$', 'account_password_remind_view'),
-    (r'^account/password/remind/success/$', 'account_password_remind_success_view'),
+    (r'^account/password/remind/success/$', 'index_view',
+        {'message': 'New auto-generated password was sent to you.'}),
 
     (r'^account/modify/$', 'account_modify_view'),
 
     (r'^account/abstracts/$', 'abstracts_view'),
     (r'^account/abstracts/submit/$', 'abstracts_submit_view'),
-    (r'^account/abstracts/submit/failed/$', 'abstracts_submit_failed_view'),
+    (r'^account/abstracts/submit/failed/$', 'abstracts_view',
+        {'message': 'The same abstract was already submitted.'}),
     (r'^account/abstracts/modify/(\d+)/$', 'abstracts_modify_view'),
-    (r'^account/abstracts/modify/failed/$', 'abstracts_modify_failed_view'),
+    (r'^account/abstracts/modify/failed/$', 'abstracts_view',
+        {'message': 'The same abstract was already submitted.'}),
     (r'^account/abstracts/delete/(\d+)/$', 'abstracts_delete_view'),
 )
 
 def _render_to_response(page, request, args=None):
-    return render_to_response(page, RequestContext(request, args), mimetype="text/html")
+    return render_to_response(page, RequestContext(request, args))
+
+def _render_template(request, **args):
+    return _render_to_response(args.get('template'), request)
 
 def handler404(request):
     return _render_to_response('errors/404.html', request)
@@ -70,30 +79,6 @@ def handler500(request):
 
 def index_view(request, **args):
     return _render_to_response('base.html', request, args)
-
-def topics_view(request, **args):
-    return _render_to_response('content/topics.html', request)
-
-def keynote_view(request, **args):
-    return _render_to_response('content/keynote.html', request)
-
-def committees_view(request, **args):
-    return _render_to_response('content/committees.html', request)
-
-def submission_view(request, **args):
-    return _render_to_response('content/submission.html', request)
-
-def venue_view(request, **args):
-    return _render_to_response('content/venue.html', request)
-
-def payment_view(request, **args):
-    return _render_to_response('content/payment.html', request)
-
-def accommodation_view(request, **args):
-    return _render_to_response('content/accommodation.html', request)
-
-def travel_view(request, **args):
-    return _render_to_response('content/travel.html', request)
 
 def account_login_view(request, **args):
     next = request.REQUEST.get('next', '/events/esco-2010/')
@@ -138,9 +123,6 @@ def account_delete_view(request, **args):
     else:
         return _render_to_response('account/delete.html', request, args)
 
-def account_delete_success_view(request, **args):
-    return index_view(request, message="Your account has been removed.")
-
 def account_create_view(request, **args):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -164,9 +146,6 @@ def account_create_view(request, **args):
         form = RegistrationForm()
 
     return _render_to_response('account/create.html', request, {'form': form})
-
-def account_create_success_view(request, **args):
-    return account_login_view(request, registred=True)
 
 def account_password_change_view(request, **args):
     if request.method == 'POST':
@@ -194,9 +173,6 @@ def account_password_change_view(request, **args):
 
     return _render_to_response('password/change.html', request, {'form': form})
 
-def account_password_change_success_view(request):
-    return index_view(request, message="Your password was successfully changed.")
-
 def account_password_remind_view(request, **args):
     if request.method == 'POST':
         form = ReminderForm(request.POST)
@@ -218,9 +194,6 @@ def account_password_remind_view(request, **args):
         form = ReminderForm()
 
     return _render_to_response('password/remind.html', request, {'form': form})
-
-def account_password_remind_success_view(request, **args):
-    return index_view(request, message="New auto-generated password was sent to you.")
 
 @login_required
 def account_modify_view(request, **args):
@@ -310,7 +283,8 @@ def account_modify_view(request, **args):
 @login_required
 def abstracts_view(request, **args):
     return _render_to_response('abstracts/abstracts.html', request,
-        {'abstracts': UserAbstract.objects.filter(user=request.user)})
+        {'abstracts': UserAbstract.objects.filter(user=request.user),
+         'message': args.get('message', None)})
 
 @login_required
 def abstracts_submit_view(request, **args):
@@ -355,10 +329,6 @@ def abstracts_submit_view(request, **args):
         form = UploadAbstractForm()
 
     return _render_to_response('abstracts/submit.html', request, {'form': form, 'text': 'Submit'})
-
-@login_required
-def abstracts_submit_failed_view(request, **args):
-    return _render_to_response('abstracts/failed.html', request)
 
 @login_required
 def abstracts_modify_view(request, abstract_id, **args):
@@ -409,10 +379,6 @@ def abstracts_modify_view(request, abstract_id, **args):
         form = ModifyAbstractForm({'abstract_title': abstract.title})
 
     return _render_to_response('abstracts/submit.html', request, {'form': form, 'text': 'Modify'})
-
-@login_required
-def abstracts_modify_failed_view(request, **args):
-    return _render_to_response('abstracts/failed.html', request)
 
 @login_required
 def abstracts_delete_view(request, abstract_id, **args):
